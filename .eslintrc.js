000622823887974ac5591f11b2bc9a5965cf0406
kimhayeon <k.hayeon00@gmail.com>
2024-09-01 22:38:02 +0900

    init: 프로젝트 세팅
    Co-authored-by: kuurong <s10y06@naver.com>

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'prettier/prettier': 'error',
+    'import/extensions': [
+      'error',
+      'never',
+      {
+        js: 'never',
+        ts: 'never'
+      }
+    ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: ['builtin', 'external', 'parent', 'sibling', 'type'],
+        pathGroups: [
+          {
+            pattern: '@/types/**',
+            group: 'type'
+          },
+          {
+            pattern: '**/model',
+            group: 'type'
+          },
+          {
+            pattern: '@/**',
+            group: 'parent'
+          }
+        ],
+        pathGroupsExcludedImportTypes: [],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ],
+    'import/no-unresolved': [2, { ignore: ['@/'] }]
+  }
+};
