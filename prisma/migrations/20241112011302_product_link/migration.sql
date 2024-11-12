7862ed9576a7ef0a05bae3c7f0aca4546cc00d8d
kuurong <s10y06@naver.com>
2024-11-12 10:27:28 +0900

    MODU-45 feat: 크롤링 상품 Prisma DB 저장 구현

---
+++ prisma/migrations/20241112011302_product_link/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "link" TEXT NOT NULL DEFAULT '';
--- src/crawler/crawler.module.ts
+++ src/crawler/crawler.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CrawlerController } from './crawler.controller';
 import { CrawlerService } from './crawler.service';
+import { PrismaModule } from '@/prisma/prisma.module';
 
 @Module({
+  exports: [CrawlerService],
+  imports: [PrismaModule],
   controllers: [CrawlerController],
   providers: [CrawlerService]
 })
