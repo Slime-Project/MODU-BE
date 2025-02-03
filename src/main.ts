7735c9eb9953a17d07d98f78d9c32cade8b459a0
kimhayeon <k.hayeon00@gmail.com>
2025-02-03 16:58:32 +0900

    fix: 배포

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.listen(3000, '0.0.0.0');
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
